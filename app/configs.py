d192b1dd6a0da5735bd512f9d9b860ee354d944b
Krixsick <linusgao123@gmail.com>
2026-05-27 22:36:31 -0400

    added a procfile

--- app/configs.py
+++ app/configs.py
@@ -2,9 +2,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-TWILIO_ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
-TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
-TWILIO_PHONE_NUMBER = os.environ["TWILIO_NUMBER"]
+TELNYX_API_KEY = os.environ["TELNYX_API_KEY"]
+TELNYX_NUMBER = os.environ["TELNYX_NUMBER"]
 MY_PHONE_NUMBER = os.environ["PHONE_NUMBER"]
 
 CLAUDE_API = os.environ["ANTHROPIC_API_KEY"]
